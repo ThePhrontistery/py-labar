0d32987c917ae97c6a40b6297d1b846d431c252a
Arbona Alapont <juan.jose_arbona.alapont@capgemini.com>
2023-10-25 12:03:33 +0200

    solucion main.py

--- app/main.py
+++ app/main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
-from app.business.controllers.user_controller import get_user_by_id, router as user_router
-from app.business.controllers.user_controller import get_users
+from app.business.controllers import user_controller
 
 app = FastAPI()
 
@@ -13,9 +12,6 @@ templates = Jinja2Templates(directory="app/templates")
 # Ruta principal para renderizar la plantilla HTML
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    user_data = get_users()
-    user_id = get_user_by_id(2)
+    user_data = user_controller.get_users()
+    user_id = user_controller.get_user_by_id(2)
     return templates.TemplateResponse("index.html", {"request": request, "user_data": user_data, "user_id": user_id})
-
-# Agrega el controlador de usuarios
-app.include_router(user_router, prefix="/users", tags=["users"])
