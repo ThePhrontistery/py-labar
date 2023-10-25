01ab1482a77134e13b23497cb60ff8e63333d2d1
Paola Chocron <122936460+pchocron@users.noreply.github.com>
2023-10-25 14:56:24 +0200

    DocStrings
    Hi, I added docstrings to comply with the Zen: Explicit is better than implicit.
    and also to comply with the plural sight python course and finally for my better understanding .
    I've also changed the mook_dictionary to understand it better

--- app/business/controllers/user_controller.py
+++ app/business/controllers/user_controller.py
@@ -3,17 +3,28 @@ from fastapi import APIRouter
 router = APIRouter()
 
 mock_users = [
-    {"id": 1, "username": "Usuario1"},
-    {"id": 2, "username": "Usuario2"},
-    {"id": 3, "username": "Usuario3"},
+    {"id": 1, "username": "pchocron", "name": "Paola Chocron"},
+    {"id": 2, "username": "jjarbona", "name": "Juan Arbona"},
+    {"id": 3, "username": "andsanchez", "name": "Andrés Sánchez"},
+    {"id": 3, "username": None, "name": None},
 ]
 
 @router.get("/user_data")
 def get_users():
+    """
+    This route (endpoint) allows a client to retrieve all user data from the mock dataset. The path for this endpoint is //user_data
+    Retrieve the list of all users from the mock dataset.
+    Returns: A list of dictionaries, where each dictionary represents a user's information.
+    """
     return mock_users
 
 @router.get("/user/{user_id}")
 def get_user_by_id(user_id: int):
+    """
+    Retrieve a specific user's information by their ID.
+    Args: user_id (int): The ID of the user to retrieve.
+    Returns:  A dictionary containing the user's information or an error message if not found.
+    """
     user = next((u for u in mock_users if u["id"] == user_id), None)
     if user is None:
         return {"error": "Usuario no encontrado"}
