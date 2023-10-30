45f9410931933d19f101db2cda529d7939db9edd
Paola Chocron <122936460+pchocron@users.noreply.github.com>
2023-10-30 10:31:47 +0100

    All Devon4py have been implemented in this commit
    se han agregado nuevas librerías al pyproyect.toml lo que significa que luego de iniciar poetry shell, deben escribir el comando poetry install

---
+++ main.py
@@ -0,0 +1,15 @@
+import logging
+import uvicorn
+
+# Init Logger for this Class
+from app.common.core import get_global_settings
+from app.common.core.configuration import get_log_config
+from app.common.infra.sql_adaptors import init_db_entities, get_db_settings
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    # import entities to init
+    from app.domain.todo_management.models import *  # Type: ignore
+    init_db_entities(get_db_settings())
+    uvicorn.run("app.api:api", host="localhost", port=get_global_settings().port, log_config=get_log_config(), reload=True)
