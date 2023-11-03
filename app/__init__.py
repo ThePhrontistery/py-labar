d8b1a9d5e940996a6b44a6c26bcdac3fc317687e
Andres Sanchez Rodriguez <andres.sanchez-rodriguez@capgemini.com>
2023-11-03 14:25:54 +0100

    Fixed api init

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,7 @@ Directorio principal que contiene los módulos de la aplicación.
 ----
 This is the main directory containing the application's modules.
 """
+from app.business.router import all_router
+from app.common.core import get_api
+
+api = get_api(routers=[all_router])
