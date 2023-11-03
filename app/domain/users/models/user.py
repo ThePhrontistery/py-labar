da12d88d6f0e9f71256864ed30c4427d504ba901
Andres Sanchez Rodriguez <andres.sanchez-rodriguez@capgemini.com>
2023-11-03 14:10:31 +0100

    Fix User DB entity

--- app/domain/users/models/user.py
+++ app/domain/users/models/user.py
@@ -4,6 +4,6 @@ from app.common.base.base_entity import BaseUUIDModel
 
 # DB ENTITY
 class User(BaseUUIDModel, table=True):
-    username: str = Field(nullable=False, unique=True, index=True)
-    email: str = Field(nullable=False)
+    username: str = Field(nullable=False, index=True)
+    email: str = Field(nullable=False, index=True)
     password: str = Field(nullable=False)
