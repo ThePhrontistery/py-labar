902bd910225444d54867b64037dc7952627e1bfc
Andres Sanchez Rodriguez <andres.sanchez-rodriguez@capgemini.com>
2023-11-03 14:10:52 +0100

    Removed unused util resize_image

--- app/common/utils.py
+++ app/common/utils.py
@@ -26,18 +26,3 @@ def haversine_distance(lat1, lon1, lat2, lon2):
     c = 2 * asin(sqrt(a))
     return c * EARTH_RADIUS_KM
 
-
-def resize_image(data: BinaryIO | bytes, size: int) -> bytes:
-    from PIL import Image
-
-    if isinstance(data, bytes):
-        data = io.BytesIO(data)
-
-    # Resize the image
-    image = Image.open(data)
-    image.thumbnail((size, size))
-
-    # Create the bytes output stream and return it
-    os = io.BytesIO()
-    image.save(os, image.format)
-    return os.getvalue()
