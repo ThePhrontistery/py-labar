83c40ad1e34b5c1205ea6b0476b50ed0455804b6
Andres Sanchez Rodriguez <andres.sanchez-rodriguez@capgemini.com>
2023-11-06 11:50:50 +0100

    First index.html

--- app/business/users/controllers/user_views.py
+++ app/business/users/controllers/user_views.py
@@ -1,13 +1,8 @@
 import logging
-import uuid
-from datetime import datetime
-from typing import Optional
 
-from fastapi import APIRouter, Depends, Request, Form, Response
+from fastapi import APIRouter, Request, Form
 from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
-
-from app.business.users.services.user import UserService
 
 templates = Jinja2Templates(directory="templates")
 router = APIRouter(prefix="/users")
@@ -18,3 +13,10 @@ logger = logging.getLogger(__name__)
 @router.get("/")
 async def home_page(request: Request):
     return templates.TemplateResponse('index.html', {"request": request})
+
+
+@router.post("/login")
+async def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    response = RedirectResponse(url="/home", status_code=status.HTTP_303_SEE_OTHER)
+
+    return response
--- templates/index.html
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Hola Mundo</title>
-</head>
-<body>
-    <h1>Hola Mundo</h1>
-</body>
-</html>
